cba095930a8882e5115eed7cbde094cb8a32c1e4
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-09-14 17:33:15 +0200

    Update languages.sh

--- scripts/stable-branch/languages.sh
+++ scripts/stable-branch/languages.sh
@@ -51,33 +51,33 @@
     text_1_1="Do you wish to install these packages (y/n)?"
 
     #select-your-os
-    text_2="$text_1"
+    text_2="Installation of SOLIDWORKS - Version 0.4"
     text_2_1="Select your Linux distribution!"
     text_2_2="Choose one of the following options:"
 
     #archlinux-1
-    text_3="$text_1"
+    text_3="$text_2"
     text_3_1="If you have enabled multilib repository?"
     text_3_2="$text_2_2"
     text_3_3="Yes"
     text_3_4="No"
 
     #select-your-path
-    text_4="$text_1"
+    text_4="$text_2"
     text_4_1="Choose setup type!"
     text_4_2="Choose the kind of setup that best suits your needs."
     text_4_3="Standard - Install SOLIDWORKS into your home folder. (/home/YOUR-USERNAME/.wineprefixes/solidworks)"
     text_4_4="Custom - Install SOLIDWORKS to another place."
 
     #select-your-path-custom
-    text_5="$text_1"
+    text_5="$text_2"
     text_5_1="Description - Configure the installation location!"
     text_5_2="Now you have to determine where you want to install SOLIDWORKS and then the .solidworks folder will be created for you automatically. For examlble you can install it on a external usb-drive: /run/media/user/usb-drive/wine/.solidworks or you install it into your home folder: /home/YOUR-USERNAME/.wineprefixes/solidworks)."
     text_5_3="Enter the installation path for SOLIDWORKS:"
-    text_5_4="$text_1"
+    text_5_4="$text_2"
 
     #program-exit
-    text_6="$text_1"
+    text_6="$text_2"
     text_6_1="SOLIDWORKS is completed."
     text_6_2="The SOLIDWORKS installation is complete and you can now use it for your projects."
 }
@@ -96,30 +96,30 @@
 
     
     #select-your-os
-    text_2="$text_1"
+    text_2="Installation von SOLIDWORKS - Version 0.4"
     text_2_1="Wählen Sie Ihre Linux-Distribution aus!"
     text_2_2="Wählen Sie eine der folgenden Optionen:"
 
     #archlinux-1
-    text_3="$text_1"
+    text_3="$text_2"
     text_3_1="Haben Sie das Multilib-Repository aktiviert?"
     text_3_2="$text_2_2"
     text_3_3="Ja"
     text_3_4="Nein"
 
     #select-your-path
-    text_4="$text_1"
+    text_4="$text_2"
     text_4_1="Wählen Sie den Setup-Typ aus!"
     text_4_2="Wählen Sie die Art der Einrichtung, die Ihren Anforderungen am besten entspricht."
     text_4_3="Standard – Installieren Sie SOLDIWORKS in Ihrem Benutzerordner. (/home/IHR-BENUTZERNAME/.wineprefixes/solidworks)"
     text_4_4="Benutzerdefiniert – Installieren Sie SOLIDWORKS an einem anderen Ort."
 
     #select-your-path-custom
-    text_5="$text_1"
+    text_5="$text_2"
     text_5_1="Beschreibung - Konfigurieren Sie den Installationsort!"
     text_5_2="Jetzt müssen Sie festlegen, wohin Sie SOLIDWORKS installieren möchten, und dann wird der .solidworks-Ordner automatisch für Sie erstellt. Sie können es beispielsweise auf einem externen USB-Laufwerk installieren: /run/media/user/usb-drive/wine/.solidworks oder Sie installieren es in Ihrem Home-Ordner: /home/IHR-BENUTZERNAME/.wineprefixes/solidworks)."
     text_5_3="Geben Sie den Installationspfad für SOLIDWORKS ein:"
-    text_5_4="$text_1"
+    text_5_4="$text_2"
 
     #program-exit
     text_11="$text_1"
