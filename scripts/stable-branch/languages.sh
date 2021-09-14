8100e34a4f7b0d497df03b4f516a3ae14fd32c1d
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-09-14 17:28:26 +0200

    Update languages.sh

--- scripts/stable-branch/languages.sh
+++ scripts/stable-branch/languages.sh
@@ -117,14 +117,14 @@
     #select-your-path-custom
     text_5="$text_1"
     text_5_1="Beschreibung - Konfigurieren Sie den Installationsort!"
-    text_5_2="Jetzt müssen Sie festlegen, wohin Sie Autodesk Fusion 360 installieren möchten, und dann wird der .fusion360-Ordner automatisch für Sie erstellt. Sie können es beispielsweise auf einem externen USB-Laufwerk installieren: /run/media/user/usb-drive/wine/.fusion360 oder Sie installieren es in Ihrem Home-Ordner: /home/IHR-BENUTZERNAME/.wineprefixes/fusion360)."
-    text_5_3="Geben Sie den Installationspfad für Autodesk Fusion 360 ein:"
+    text_5_2="Jetzt müssen Sie festlegen, wohin Sie SOLIDWORKS installieren möchten, und dann wird der .solidworks-Ordner automatisch für Sie erstellt. Sie können es beispielsweise auf einem externen USB-Laufwerk installieren: /run/media/user/usb-drive/wine/.solidworks oder Sie installieren es in Ihrem Home-Ordner: /home/IHR-BENUTZERNAME/.wineprefixes/solidworks)."
+    text_5_3="Geben Sie den Installationspfad für SOLIDWORKS ein:"
     text_5_4="$text_1"
 
     #program-exit
     text_11="$text_1"
-    text_11_1="Autodesk Fusion 360 ist abgeschlossen."
-    text_11_2="Die Installation von Autodesk Fusion 360 ist abgeschlossen und Sie können es nun für Ihre Projekte verwenden."
+    text_11_1="SOLIDWORKS ist abgeschlossen."
+    text_11_2="Die Installation von SOLIDWORKS ist abgeschlossen und Sie können es nun für Ihre Projekte verwenden."
 }
 
 select-language
