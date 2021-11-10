82ff59e72f1b8df55ebfdfe947ef274a8ea7f3bf
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-11-10 17:13:56 +0100

    Add files via upload

---
+++ files/scripts/stable-branch/data/locale/zh-CN/locale-zh.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+####################################################################################################
+# Name:         Solidworks - Setup Wizard (Linux)                                                  #
+# Description:  This is the Chinese translation for the Setup Wizard.                              #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2021                                                                          #
+# Time/Date:    16:00/10.11.2021                                                                   #
+# Version:      1.0                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS ARE ARRANGED HERE:                                                                                                                          #
+###############################################################################################################################################################
+
+# License-checkbox
+text_license_checkbox="Wǒ yǐ yuèdú tiáokuǎn hé tiáojiàn bìng jiēshòu tāmen."
+
+# Select
+text_select="Xuǎnzé"
+
+# Version (Standard, Flatpak or Snap)
+text_version="Bǎnběn"
+text_standard="Biāozhǔn"
+text_flatpak="Flatpak (Shíyàn xìng de)"
+text_snap="Snap (Shíyàn xìng de)"
+
+# Linux distribution
+text_linux_distribution="Linux fāxíng bǎn"
+
+# Installation location
+text_installation_location="Ānzhuāng wèizhì"
+
+# Installation location - Standard
+text_installation_location_standard="Biāozhǔn - Jiāng Solidworks ānzhuāng dào nín de zhǔ wénjiàn jiā zhōng. Lìrú: /home/username/.wineprefixes/solidworks"
+
+# Installation location - Custom
+text_installation_location_custom="Zì dìngyì - Jiāng Solidworks ānzhuāng dào qítā wèizhì. Lìrú: /run/media/username/usb-drive/.wineprefixes/solidworks"
+
+# Select a location with a file browser
+text_select_location_custom="Xuǎnzé Solidworks ānzhuāng wèizhì:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="Wǒ yǐjīng gēnggǎile Solidworks de lùjìng!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Shìfǒu yào bǎocún gēnggǎi?"
+
+# Abort the program
+text_abort="Nín zhēn de yāo zhōngzhǐ ānzhuāng ma?"
+
+# Program-Exit
+text_completed_installation="Solidworks ānzhuāng wánchéng."
+
+# Select a option - new_modify_deinstall
+text_select_option="Xuǎnxiàng"
+
+# Option 1
+text_select_option_1="Bùfèn huò suǒyǒu zǔjiàn de quánxīn ānzhuāng"
+
+# Option 2
+text_select_option_2="Gēngxīn huò xiūfù xiàn yǒu ānzhuāng"
+
+# Option 3
+text_select_option_3="Xièzài suǒyǒu Solidworks zǔjiàn"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="Wǒ xiànzài yǐjīng kàn dàole nǎxiē ānzhuāng lùjìng kěyòng, bìng jiàng zàixià yībù zhōng zhǐdìng bùtóng de lùjìng!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="Wǒ cóng xiàn yǒu de Solidworks ānzhuāng zhōng jì xià huò fùzhìle zhèngquè de lùjìng!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="Wǒ cóng xiàn yǒu de Solidworks ānzhuāng zhōng jì xià huò fùzhìle zhèngquè de lùjìng, ránhòu zài cǐ chù shān chú liǎo cǐ lùjìng!"
+
+# Question - Deinstall a exist Solidworks installation
+text_deinstall_question="Shìfǒu yào bǎocún gēnggǎi bìng shānchú zhèngquè de xiàn yǒu Solidworks ānzhuāng?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Xuǎnzé nín xiàn yǒu de Solidworks ānzhuāng:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="Solidworks xièzài wánchéng."
+
+# Error
+text_error="Yīgè yìliào zhī wài de wèntí fāshēngle!"
+
