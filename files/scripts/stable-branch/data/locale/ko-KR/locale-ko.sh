02074e6270fcdccb57f57ead96df3674a52a8fd3
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-11-10 17:11:14 +0100

    Add files via upload

---
+++ files/scripts/stable-branch/data/locale/ko-KR/locale-ko.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+####################################################################################################
+# Name:         Solidworks - Setup Wizard (Linux)                                                  #
+# Description:  This is the Korean translation for the Setup Wizard.                               #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2021                                                                          #
+# Time/Date:    16:00/10.11.2021                                                                   #
+# Version:      1.0                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS ARE ARRANGED HERE:                                                                                                                          #
+###############################################################################################################################################################
+
+# License-checkbox
+text_license_checkbox="Iyong-yaggwan-eul ilg-eoss-eumyeo ie dong-uihabnida."
+
+# Select
+text_select="Seontaeghada"
+
+# Version (Standard, Flatpak or Snap)
+text_version="Beojeon"
+text_standard="Gijun"
+text_flatpak="Flatpak (Silheomjeog)"
+text_snap="Snap (Silheomjeog)"
+
+# Linux distribution
+text_linux_distribution="Linugseu baepopan"
+
+# Installation location
+text_installation_location="Seolchi wichi"
+
+# Installation location - Standard
+text_installation_location_standard="Pyojun - Hom poldeoe Solidworks eul seolchihabnida. Ye: /home/username/.wineprefixes/solidworks"
+
+# Installation location - Custom
+text_installation_location_custom="Sayongja jeong-ui - Solidworks eul daleun wichie seolchihabnida. Ye: /run/media/username/usb-drive/.wineprefixes/solidworks"
+
+# Select a location with a file browser
+text_select_location_custom="Solidworks seolchi wichileul seontaeghasibsio:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="Solidworks ui gyeongloleul byeongyeonghaessseubnida!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="Byeong-yeong sahang-eul jejanghasigessseubnikka?"
+
+# Abort the program
+text_abort="Seolchileul jungdanhasigessseubnikka?"
+
+# Program-Exit
+text_completed_installation="Solidworks seolchiga wanlyodoeeossseubnida."
+
+# Select a option - new_modify_deinstall
+text_select_option="Obsyeon"
+
+# Option 1
+text_select_option_1="Ilbu ttoneun modeun guseong yosoui sae seolchi"
+
+# Option 2
+text_select_option_2="Gijon seolchi eobdeiteu ttoneun boggu"
+
+# Option 3
+text_select_option_3="Modeun Solidworks guseong-yoso jegeo"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="Ije sayong ganeunghan seolchi gyeongloleul hwag-inhaess-eumyeo da-eum dangyeeseo daleun gyeongloleul jijeonghal geos-ibnida!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="Gijon Solidworks seolchieseo olbaleun gyeongloleul giloghageona bogsahaessseubnida!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="Gijon Solidworks seolchieseo olbaleun gyeongloleul giloghageona bogsahago i gyeongloleul sagjehan da-eum yeogieseo sagjehaessseubnida!"
+
+# Question - Deinstall a exist Solidworks installation
+text_deinstall_question="Byeongyeong sahang-eul jeojanghago olbaleun gijon Solidworks seolchileul sagjehasigessseubnikka?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Gijon Solidworks seolchileul seontaeghasibsio:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="Solidworks ui jegeoga wanlyodoeeossseubnida."
+
+# Error
+text_error="Yesangchi moshan olyuga balsaenghaessseubnida!"
