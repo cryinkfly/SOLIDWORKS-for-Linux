a1117d6652ada8be7d933f0b730bc8475437cd72
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2021-11-10 16:53:18 +0100

    Add files via upload

---
+++ files/scripts/stable-branch/data/locale/es-ES/locale-es.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+####################################################################################################
+# Name:         Solidworks - Setup Wizard (Linux)                                                  #
+# Description:  This is the Spanish translation for the Setup Wizard.                              #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2021                                                                          #
+# Time/Date:    16:00/10.11.2021                                                                   #
+# Version:      1.0                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS ARE ARRANGED HERE:                                                                                                                          #
+###############################################################################################################################################################
+
+# License-checkbox
+text_license_checkbox="He leído los términos y condiciones y los acepto."
+
+# Select
+text_select="Seleccione"
+
+# Version (Standard, Flatpak or Snap)
+text_version="Versión"
+text_standard="Standard"
+text_flatpak="Flatpak (Experimental)"
+text_snap="Snap (Experimental)"
+
+# Linux distribution
+text_linux_distribution="Distribución de Linux"
+
+# Installation location
+text_installation_location="Ubicación de la instalación"
+
+# Installation location - Standard
+text_installation_location_standard="Estándar - Instale Solidworks en su carpeta de inicio. Por ejemplo: /home/username/.wineprefixes/solidworks"
+
+# Installation location - Custom
+text_installation_location_custom="Personalizado: instala Solidworks en otro lugar. Por ejemplo: /run/media/username/usb-drive/.wineprefixes/solidworks"
+
+# Select a location with a file browser
+text_select_location_custom="Seleccione una ubicación para su instalación de Solidworks:"
+
+# Checkbox - desktop-launcher-custom
+text_desktop_launcher_custom_checkbox="¡He cambiado la ruta de Solidworks!"
+
+# Question - desktop-launcher-custom
+text_desktop_launcher_custom_question="¿Quieres guardar tus cambios?"
+
+# Abort the program
+text_abort="¿Realmente quieres abortar la instalación?"
+
+# Program-Exit
+text_completed_installation="Se completa la instalación de Autodesk Fusion 360."
+
+# Select a option - new_modify_deinstall
+text_select_option="Opción"
+
+# Option 1
+text_select_option_1="Nueva instalación de algunos o todos los componentes"
+
+# Option 2
+text_select_option_2="Update or Repair a existing installation"
+
+# Option 3
+text_select_option_3="Desinstale todos los componentes de Solidworks"
+
+# New installation-checkbox - new_modify_deinstall
+text_new_installation_checkbox="¡Ahora he visto qué rutas de instalación están disponibles y especificaré una ruta diferente en el siguiente paso!"
+
+# Edit installation-checkbox - new_modify_deinstall
+text_edit_installation_checkbox="¡Escribí o copié la ruta correcta de una instalación existente de Solidworks!"
+
+# Deinstall-checkbox - new_modify_deinstall
+text_deinstall_checkbox="¡Escribí o copié la ruta correcta de una instalación existente de Solidworks y eliminé esta ruta aquí!"
+
+# Question - Deinstall a exist Solidworks installation
+text_deinstall_question="¿Desea guardar sus cambios y eliminar la instalación correcta de Solidworks existente?"
+
+# Select a location with a file browser - new_modify_deinstall
+text_select_location_deinstall="Seleccione su instalación existente de Solidworks:"
+
+# Program-Exit - new_modify_deinstall
+text_completed_deinstallation="Se completa la desinstalación de Solidworks."
+
+# Error
+text_error="¡Ocurrió un error inesperado!"
+
