3c3573fea6a72085310e583a2bfd50f304fbdccb
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2023-03-12 18:53:31 +0100

    Update locale-en.sh

--- files/builds/stable-branch/locale/en-US/locale-en.sh
+++ files/builds/stable-branch/locale/en-US/locale-en.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+####################################################################################################
+# Name:         SOLIDWORKS - Setup Wizard (Linux)                                                  #
+# Description:  This is the English translation for the Setup Wizard.                              #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2023                                                                          #
+# Time/Date:    19:00/12.03.2023                                                                   #
+# Version:      1.1                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS FOR SOLIDWORKS ARE ARRANGED HERE:                                                                                                           #
+###############################################################################################################################################################
+
+SP_SELECT_OS_VERSION_SUBTITLE="Linux distribution - Configuration"
+SP_SELECT_OS_VERSION_TEXT="Please select your Linux distribution to install the required packages for the installation:"
+
+SP_EXIT_SUBTITLE="Installation - Completed"
+SP_EXIT_TEXT="The SOLIDWORKS installation is complete and you can now use it for your projects."
+
+###############################################################################################################################################################
