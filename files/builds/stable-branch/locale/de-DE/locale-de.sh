ab37fb30993d8880cf70697f4e7b2bb8831cc537
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2023-03-12 18:55:17 +0100

    Update locale-de.sh

--- files/builds/stable-branch/locale/de-DE/locale-de.sh
+++ files/builds/stable-branch/locale/de-DE/locale-de.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+####################################################################################################
+# Name:         SOLIDWORKS - Setup Wizard (Linux)                                                  #
+# Description:  This is the German translation for the Setup Wizard.                               #
+# Author:       Steve Zabka                                                                        #
+# Author URI:   https://cryinkfly.com                                                              #
+# License:      MIT                                                                                #
+# Copyright (c) 2020-2023                                                                          #
+# Time/Date:    19:00/12.03.2023                                                                   #
+# Version:      1.1                                                                                #
+####################################################################################################
+
+###############################################################################################################################################################
+# ALL DEFINITIONS FOR SOLIDWORKS ARE ARRANGED HERE:                                                                                                           #
+###############################################################################################################################################################
+
+SP_SELECT_OS_VERSION_SUBTITLE="Linux-Distribution - Konfiguration"
+SP_SELECT_OS_VERSION_TEXT="Bitte wählen Sie Ihre Linux-Distribution aus, um die erforderlichen Pakete für die Installation zu installieren:"
+
+SP_EXIT_SUBTITLE="Installation - Abgeschlossen"
+SP_EXIT_TEXT="Die Installation von SOLIDWORKS ist abgeschlossen und Sie können es jetzt für Ihre Projekte verwenden."
+
+###############################################################################################################################################################
