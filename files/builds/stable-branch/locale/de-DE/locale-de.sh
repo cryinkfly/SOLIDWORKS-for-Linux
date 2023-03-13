84c42ac4fc4a7d3c5ffde5a2d9eea779bf845921
Steve Zabka <79079633+cryinkfly@users.noreply.github.com>
2023-03-13 10:55:13 +0100

    Update locale-de.sh

--- files/builds/stable-branch/locale/de-DE/locale-de.sh
+++ files/builds/stable-branch/locale/de-DE/locale-de.sh
@@ -7,13 +7,21 @@
 # Author URI:   https://cryinkfly.com                                                              #
 # License:      MIT                                                                                #
 # Copyright (c) 2020-2023                                                                          #
-# Time/Date:    19:00/12.03.2023                                                                   #
-# Version:      1.1                                                                                #
+# Time/Date:    11:00/13.03.2023                                                                   #
+# Version:      1.2                                                                                #
 ####################################################################################################
 
 ###############################################################################################################################################################
 # ALL DEFINITIONS FOR SOLIDWORKS ARE ARRANGED HERE:                                                                                                           #
 ###############################################################################################################################################################
+
+SP_LICENSE_ERROR="Achtung! - Die Lizenzvereinbachung wurde nicht akzeptiert! \n Bitte lesen und aktzeptieren Sie die Lizenzvereinbachung, um mit der Installation fortfahren (JA) zu können."
+
+SP_SELECT_SOLIDWORKS_VERSION_SUBTITLE="SOLIDWORKS - Versionsauswahl"
+SP_SELECT_SOLIDWORKS_VERSION_TEXT="Bitte wählen Sie Ihre SOLIDWORKS-Version aus, um fortfahren zu können:"
+
+SP_SELECT_SOLIDWORKS_SUBTITLE="SOLIDWORKS - Installationsdatei (EXE) auswählen"
+SP_SELECT_SOLIDWORKS_TEXT="Bitte navigieren Sie in den Ordner, wo sich die SOLDIWORKS-Installationsdatei (EXE) befindet und wählen Sie die Datei aus."
 
 SP_SELECT_OS_VERSION_SUBTITLE="Linux-Distribution - Konfiguration"
 SP_SELECT_OS_VERSION_TEXT="Bitte wählen Sie Ihre Linux-Distribution aus, um die erforderlichen Pakete für die Installation zu installieren:"
